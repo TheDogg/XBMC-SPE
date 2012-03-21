12fb2cd2e38ce4ec5bedac4009d32b3b35edc796
TheDogg <sebastien.gascon@gmail.com>
2012-03-21 12:06:59 -0400

    Creation of template file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**********************************************************************/
+/* Project Name.: XBMC-SPE							*/
+/* GIT .........: https://github.com/TheDogg/XBMC-SPE			*/
+/* File Name....: index.php							*/
+/* Author.......: Sebastien Gascon						*/
+/* Author Email.: sebastien.gascon@gmail.com				*/
+/* Created On...: 21/03/2012 11:39:54 AM					*/
+/**********************************************************************/
+
+?>
